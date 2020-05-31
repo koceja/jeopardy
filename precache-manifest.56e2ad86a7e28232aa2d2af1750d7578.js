d5c0f8bb7b2eb29d7204348b76d4a00f7e436367
koceja <dkoceja@live.unc.edu>
2020-05-31 06:05:19 -0400

    Updates

---
+++ precache-manifest.56e2ad86a7e28232aa2d2af1750d7578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b5f62f67e8aac4189e02eba01a9f8b",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "39deb2cce2f05ee49258",
+    "url": "/jeopardy/static/css/main.8cdc4aa0.chunk.css"
+  },
+  {
+    "revision": "bb78d59c7d0f3ce9420c",
+    "url": "/jeopardy/static/js/2.4c1929c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jeopardy/static/js/2.4c1929c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39deb2cce2f05ee49258",
+    "url": "/jeopardy/static/js/main.f93b8314.chunk.js"
+  },
+  {
+    "revision": "a900741abb216ea68175",
+    "url": "/jeopardy/static/js/runtime-main.83f780aa.js"
+  }
+]);
